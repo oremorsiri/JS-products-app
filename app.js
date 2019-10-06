24406b9c2d939d86c7d0e3708ea0d72088d60474
Iris <irisromeropiquer@gmail.com>
2019-10-06 13:41:22 +0200

    class UI created, addProduct and resetForm created and implemented into product-form

--- app.js
+++ app.js
@@ -4,15 +4,40 @@ class Product {
         this.price = price;
         this.year = year;
     }
-} 
+}
+
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+        <div class="card text-center mb-4">
+            <div class="card-body">
+                <strong>Product Name</strong>: ${product.name}
+                <strong>Product Price</strong>: ${product.price}
+                <strong>Product Year</strong>: ${product.year}
+            </div>
+        </div>
+        `;
+        productList.appendChild(element);
+    }
+
+    resetForm() {
+        document.getElementById('product-form').reset()
+    }
+}
 
 // DOM Events
-document.getElementById('product-form').addEventListener('submit', function(e) {
+document.getElementById('product-form').addEventListener('submit', function (e) {
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
     const year = document.getElementById('year').value;
 
     const product = new Product(name, price, year)
+
+    const ui = new UI();
+    ui.addProduct(product);
+    ui.resetForm();
 
     e.preventDefault();
 })
